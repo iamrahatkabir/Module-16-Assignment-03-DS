299b06582814a48342477fd8ca245e21cf1a1254
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-25 02:18:46 +0600

    Solved

---
+++ Elimination.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+
+    while(n--)
+    {
+        string s;
+        cin >> s;
+
+        stack<char> st;
+
+        for(char c : s)
+        {
+            if(st.empty())
+            {
+                st.push(c);
+            }
+            else if(c == '0')
+            {
+                st.push(c);
+            }
+            else if(!st.empty() && c == '1')
+            {
+                if(st.top() == '0')
+                {
+                    st.pop();
+                }
+                else{
+                    st.push(c);
+                }
+            }
+        }
+        if(st.empty())
+        {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
